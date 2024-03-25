cf65101871ecc4b0fc8f8f297f52ccbd7cdc5823
xuhao <24390149@qq.com>
2024-03-25 18:54:42 +0800

    用户-接口信息模块

---
+++ src/main/java/com/haoapi/mapper/UserInterfaceInfoMapper.java
@@ -0,0 +1,18 @@
+package com.haoapi.mapper;
+
+import com.haoapi.model.entity.UserInterfaceInfo;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author XuHao
+* @description 针对表【user_interface_info(用户调用接口关系)】的数据库操作Mapper
+* @createDate 2024-03-25 18:50:52
+* @Entity com.haoapi.model.entity.UserInterfaceInfo
+*/
+public interface UserInterfaceInfoMapper extends BaseMapper<UserInterfaceInfo> {
+
+}
+
+
+
+
